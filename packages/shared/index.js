ade2d3bbff437b7311dd09ed9cccfd5bf1c139a9
Fardin Karimi <fardin_karimi_mgst@yahoo.com>
2019-03-04 00:30:56 +0100

    remove unused code

--- packages/shared/index.js
+++ packages/shared/index.js
@@ -1,6 +1,3 @@
-export const isUserAllowed = (user, allowedUserTypes) =>
-  user && allowedUserTypes && allowedUserTypes.includes(user.type)
-
 const getFirstLetter = text => (text ? text[0] : '')
 
 export const getInitials = (firstName, lastName) =>
