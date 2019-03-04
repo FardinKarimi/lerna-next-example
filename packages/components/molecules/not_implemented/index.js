ed047a6f2e3d591e40bba56fa913be55b2a07f44
Fardin Karimi <fardin_karimi_mgst@yahoo.com>
2019-03-04 19:27:44 +0100

    refactor

--- packages/components/molecules/not_implemented/index.js
+++ packages/components/molecules/not_implemented/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
+
 import withText from '../../_hocs/with_text'
 import H1 from '../../atoms/h1'
 import Text from '../../atoms/text'
-import Link from '../../atoms/link'
+import Section from '../../atoms/section'
 
 const NotImplementedPage = ({ texts }) => (
-  <>
+  <Section>
     <H1>{texts.oops}</H1>
     <Text>{texts.notImplemented}</Text>
-    <Link href="/">{texts.home}</Link>
-  </>
+  </Section>
 )
 
 export default withText(NotImplementedPage)
