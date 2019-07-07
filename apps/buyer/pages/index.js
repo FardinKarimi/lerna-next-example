083d4f54cdfc34a7a7be6afb20167dfca0ae60c9
Fardin Karimi <fardin@mobilabsolutions.com>
2019-07-07 20:39:24 +0200

    stateless home page

--- apps/buyer/pages/index.js
+++ apps/buyer/pages/index.js
@@ -5,24 +5,16 @@ import ComponentWithMenu from '@company/components/organisms/component_with_menu
 import Shop from '../templates/shop'
 import BuyerMenu from '../templates/buyer_menu'
 
-class MainPage extends React.Component {
-  static async getInitialProps(ctx) {
-    const baseUrl = ctx.req ? `http://${ctx.req.hostname}` : ''
-    const articlesResponse = await fetch(`${baseUrl}/api/v1/articles`)
-    const articles = await articlesResponse.json()
-    return { articles }
-  }
+const MainPage = ({ articles }) => {
+  const renderShop = () => <Shop articles={articles} />
+  return <ComponentWithMenu renderMenu={BuyerMenu} renderContent={renderShop} />
+}
 
-  renderShop = () => <Shop articles={this.props.articles} />
-
-  render() {
-    return (
-      <ComponentWithMenu
-        renderMenu={BuyerMenu}
-        renderContent={this.renderShop}
-      />
-    )
-  }
+MainPage.getInitialProps = async ctx => {
+  const baseUrl = ctx.req ? `http://${ctx.req.hostname}` : ''
+  const articlesResponse = await fetch(`${baseUrl}/api/v1/articles`)
+  const articles = await articlesResponse.json()
+  return { articles }
 }
 
 export default MainPage
