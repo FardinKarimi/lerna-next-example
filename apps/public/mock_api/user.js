0036e36a76c4df8ec323a7f4a2ea885bdae94184
Fardin Karimi <fardin@mobilabsolutions.com>
2019-07-21 23:25:34 +0200

    add mock data

---
+++ apps/public/mock_api/user.js
@@ -0,0 +1,17 @@
+export const fardin = {
+  userName: 'fardin@mobilabsolutions.com',
+  companyName: 'MobiLab Solutions GmbH.',
+  userType: 'Online Buyer',
+  firstName: 'Fardin',
+  lastName: 'Karimi',
+  imageUrl: 'https://avatars0.githubusercontent.com/u/15778261?s=460&v=4'
+}
+
+export const joao = {
+  userName: 'joao@mobilabsolutions.com',
+  companyName: 'MobiLab Solutions GmbH.',
+  userType: 'Online Seller',
+  firstName: 'Jøao',
+  lastName: 'Henriques',
+  imageUrl: 'https://avatars0.githubusercontent.com/u/1117330?s=460&v=4'
+}
