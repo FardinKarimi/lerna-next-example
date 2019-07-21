d25a541426f5d2a21ca9c67d2add9d3f0573a3d3
Fardin Karimi <fardin@mobilabsolutions.com>
2019-07-21 14:40:37 +0200

    small ui improvements

--- packages/components/organisms/component_with_menu/index.js
+++ packages/components/organisms/component_with_menu/index.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 const Wrapper = styled.div`
   font-size: 1em;
   display: flex;
+  flex-wrap: wrap;
 `
 
 const ComponentWithMenu = ({ renderMenu, renderContent }) => (
