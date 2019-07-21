be97fb20c718094fbbceabe43d3f47796960925d
Fardin Karimi <fardin@mobilabsolutions.com>
2019-07-21 14:39:41 +0200

    move user to store

--- packages/components/_contexts/user_provider/index.js
+++
@@ -1,6 +0,0 @@
-import React, { useContext } from 'react'
-
-export const UserContext = React.createContext()
-export const UserConsumer = UserContext.Consumer
-export const UserProvider = UserContext.Provider
-export default () => useContext(UserContext)
